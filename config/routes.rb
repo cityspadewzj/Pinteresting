577f01001cd43fc12db68ad2a7350d9ad4774846
Alex Wang <zijun.wang@cityspade.com>
2015-05-05 12:40:21 -0400

    added resource:pins in devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  resources :pins
+
   devise_for :users
   root "pages#home"
   get "about" => "pages#about" #create about_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+kkkk
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504155548) do
+ActiveRecord::Schema.define(version: 20150505063334) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
