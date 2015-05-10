bbcde6037356ee01dbdd555272e51b1a496aa246
Alex Wang <zijun.wang@cityspade.com>
2015-05-10 00:07:08 -0400

    FEW CHANGES IN DB AND ADDED PHOTO UPLOAD

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" } , :default_url => "https://c4.staticflickr.com/4/3425/13078131074_011f3f18bb_k.jpg"
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" } , :default_url => "https://c4.staticflickr.com/4/3425/13078131074_011f3f18bb_k.jpg"
 end
