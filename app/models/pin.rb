0a30e1bba6cd07b805e4b04470c0c09923d65e54
Alex Wang <zijun.wang@cityspade.com>
2015-05-17 14:52:28 -0400

    slightly modified pins model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,9 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" } , :default_url => "https://c4.staticflickr.com/4/3425/13078131074_011f3f18bb_k.jpg"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	
+	#Added validation to pins
+	validates :image, presence: true
+	validates :description, presence: true
+
 end
